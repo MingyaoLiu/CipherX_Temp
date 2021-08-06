95a7350a9978bcd535104c2d130e15c60d9d3080
Mingyao Liu <MingyaoLiu@users.noreply.github.com>
2021-08-06 10:46:11 -0400

    Add files via upload

---
+++ devServer.js
@@ -0,0 +1,28 @@
+
+// Include all required NPM
+var http = require('http');
+var express = require('express');
+var app = express();
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var queryString = require('querystring');
+// set up our express application
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+//Define port (prod is 80)
+var port = process.env.PORT || 8080;
+// Dev live-reload 
+var livereload = require('livereload');
+server = livereload.createServer();
+server.watch(__dirname + "/app");
+//Sending public folder
+app.use(express.static(__dirname + '/app'));
+app.get('/', function(req, res) {
+        res.sendFile('index.html');
+    });
+app.listen(port);
+console.log('Cipher X running at ' + port);
+exports = module.exports = app;
